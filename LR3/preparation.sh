cd0cdeb9feb0fc6423f3772924cca803e92ca74e
NofD <51337108+NofD@users.noreply.github.com>
2020-05-13 23:48:21 +0300

    Update preparation.sh

--- LR3/preparation.sh
+++ LR3/preparation.sh
@@ -4,8 +4,7 @@ sudo apt-get update
 sudo apt-get install libreoffice --no-install-recommends
 sudo apt-get install libreoffice-calc
 sudo apt-get install nfdump
+sudo python3 -m pip install -U pip
 sudo python3 -m pip install num2words
 sudo python3 -m pip install exceptions
 sudo python3 -m pip install python-docx
-sudo python3 -m pip install -U pip
-sudo python3 -m pip install -U matplotlib
